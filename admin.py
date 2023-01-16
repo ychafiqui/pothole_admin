e14b7d97d606bb321cde7ea720ad29f53452f146
Youssef Chafiqui <62183880+ychafiqui@users.noreply.github.com>
2023-01-16 19:11:38 +0100

    Add files via upload

--- admin.py
+++ admin.py
@@ -4,47 +4,10 @@ from streamlit_authenticator import Authenticate
 import yaml
 from yaml import SafeLoader
 import pandas as pd
+import requests
+# import re
 
-if not hasattr(st, 'already_started_server'):
-    # Hack the fact that Python modules (like st) only load once to
-    # keep track of whether this file already ran.
-    st.already_started_server = True
-
-    st.write('''
-        The first time this script executes it will run forever because it's
-        running a Flask server.
-
-        Just close this browser tab and open a new one to see your Streamlit
-        app.
-    ''')
-
-    from flask import Flask,request
-
-    app = Flask(__name__)
-
-    @app.route('/get_detections')
-    def get_detections():
-        df = pd.read_csv("detections/detections.csv")
-        json_data = df.to_json(orient='records')
-        return json_data
-
-    @app.route('/add_detections', methods=['POST'])
-    def add_detections():
-        image = request.form['image']
-        lat = request.form['lat']
-        lon = request.form['lon']
-        nb_potholes = request.form['nb_potholes']
-        date = request.form['date']
-        city = request.form['city']
-        region = request.form['region']
-        country = request.form['country']
-        df = pd.read_csv("detections/detections.csv")
-        df = pd.concat([df, pd.DataFrame({'image': [image], 'lat': [lat], 'lon': [lon], 'nb_potholes': [nb_potholes], 'date': [date], 'city': [city], 'region': [region], 'country': [country]})], ignore_index=True)
-        df.to_csv("detections/detections.csv", index=False)
-        json_data = df.to_json(orient='records')
-        return json_data
-
-    app.run()
+st.set_page_config(layout="wide")
 
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
@@ -61,7 +24,40 @@ name, authentication_status, username = authenticator.login('Login', 'main')
 
 if authentication_status:
     authenticator.logout('Logout', 'main')
-    df = pd.read_csv("detections/detections.csv")
-    st.map(df)
+    x = requests.get('http://potholeapi.pythonanywhere.com/get_detections')
+    df = pd.read_json(x.text)
+
+    # df["city"] = df['city'].apply(lambda x: re.sub('[^A-Za-z0-9]+', '', x) if x is not None else x)
+    # df["region"] = df['region'].apply(lambda x: re.sub('[^A-Za-z0-9]+', '', x))
+    # df["country"] = df['country'].apply(lambda x: re.sub('[^A-Za-z0-9]+', '', x))
+
+    st.title("Pothole Detection Admin Dashboard")
+    country_filter = st.multiselect('Country Filter', df['country'].unique())
+    df2 = df
+    if country_filter:
+        df2 = df2[df2['country'].isin(country_filter)]
+
+    region_filter = st.multiselect('Region Filter', df2['region'].unique())
+    if region_filter:
+        df2 = df2[df2['region'].isin(region_filter)]
+
+    city_filter = st.multiselect('City Filter', df2['city'].unique())
+    if city_filter:
+        df2 = df2[df2['city'].isin(city_filter)]
+
+    # date filter
+    df2.date = df2.date.map(lambda x: x.date())
+    date_filter = st.date_input('Date Filter', [df2['date'].min(), df2['date'].max()])
+    df2 = df2[(df2['date'] >= date_filter[0]) & (df2['date'] <= date_filter[1])]
+
+    st.map(df2)
+    
+    st.header("Number of potholes per city")
+    st.bar_chart(df.groupby('city')['nb_potholes'].sum())
+    st.header("Number of potholes per region")
+    st.bar_chart(df.groupby('region')['nb_potholes'].sum())
+    st.header("Number of potholes per country")
+    st.bar_chart(df.groupby('country')['nb_potholes'].sum())
+
 elif authentication_status == False:
     st.error('Username/password is incorrect')
